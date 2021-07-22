ab49cea718a6e57c413901c14e8d15b142dcc8df
Vanessa Zurbito <87353827+zurbitovanessa@users.noreply.github.com>
2021-07-22 22:44:31 +0800

    Update db.py

--- models/db.py
+++ models/db.py
@@ -137,10 +137,12 @@ db.define_table(
     Field("term_sem", type="string", length=15),
     Field("term_year", type="text", length=10)
 )
-db.define_table(
-    'good_moral_certificate',
-    Field("student_id", 'reference student', notnull=True),
-    Field("date_issued", type='date', default = request.now, requires = IS_DATE(format=('%d-%m-%Y'))),
-    Field("registrar", 'reference staff'),
-    Field("revision", type='integer', default=0)
+db.define_table('good_moral_certificate',
+   Field("student_id", 'reference student', notnull=True),
+   Field("date_issued", type='date', default = request.now, requires = IS_DATE(format=('%d-%m-%Y'))),
+   Field("year_level", type='string', length=3),
+   Field("registrar", 'reference staff'),
+   Field("signatory1", 'reference staff'),
+   Field("signatory2", 'reference staff'),
+   Field("revision", type='integer', default=0)
 )
